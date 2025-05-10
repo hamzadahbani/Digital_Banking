a5de4534c2dfa052e668c2d0d517c733a8db17be
Hamza Dahbani <dahbanihamza1@gmail.com>
2025-05-10 13:17:31 +0100

    first commit

---
+++ ebank_backend/src/main/java/net/dahbani/ebank_backend/EbankBackendApplication.java
@@ -0,0 +1,13 @@
+package net.dahbani.ebank_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbankBackendApplication.class, args);
+    }
+
+}
---
+++ ebank_backend/src/test/java/net/dahbani/ebank_backend/EbankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dahbani.ebank_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbankBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
